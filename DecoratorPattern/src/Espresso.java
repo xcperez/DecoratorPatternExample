c3ab10fd3da97546023d7e36417030e50222ee18
ncc <ncc>
2017-03-01 09:21:45 -0800

    Added implemented Beverages

---
+++ DecoratorPattern/src/Espresso.java
@@ -0,0 +1,16 @@
+
+public class Espresso extends Beverage{
+
+	public Espresso(){
+		
+		description = "Espresso";
+		
+	}
+	public double cost() {
+		// TODO Auto-generated method stub
+		return 1.99;
+	}
+	
+	
+
+}
